7d2bf6e7d33d25170cd656ec523f24ded24b9cfd
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2024-04-13 12:42:17 -0400

    Removing Unnecessary lines

--- src/app/pages/individual-garage-page/individual-garage-page.component.ts
+++ src/app/pages/individual-garage-page/individual-garage-page.component.ts
@@ -51,43 +51,7 @@ export class IndividualGaragePageComponent implements OnInit {
         this.isLoading = false;
       });
   }
-  //this.router.navigate(['history-list']);
   redirectToReservationPage(): void {
     this.router.navigate(['/reservation']);
 }
 }
-/*
-  garage: GarageDTO = {
-    GarageID: 'G001',
-    GarageName: 'Laurent-Garage',
-    Address: '3131 blvd Cote Vertu , Saint-laurent, QC H4R 1Y8',
-    Capacity: 150,
-    Occupancy: 75,
-    OpeningHours: [
-      'Monday - Friday: 7 AM - 10 PM',
-      'Saturday - Sunday: 9 AM - 6 PM',
-    ],
-    Images: [
-      'https://crescentparking.com/wp-content/uploads/2017/08/450_Mayor_4.jpg',
-    ],
-    RealTimeAvailability: true,
-    Policy:
-      'Vehicles are not permitted to remain parked on the premises for more than 24 hours. Any vehicle exceeding this limit may be subject to towing at the owner expense',
-   // LastUpdated: new Date().toISOString(),
-    Rates: {
-      HourlyRate: 5,
-      DailyRate: 20,
-      MonthlyRate: 150,
-    },
-    Facilities: {
-      HandicapAccessible: true,
-      EVChargingStations: 4,
-    },
-    VehicleTypes: [
-      VehicleType.Car,
-      VehicleType.Motorcycle,
-      VehicleType.LargeTruck,
-    ],
-    PaymentOptions: [PaymentOption.CreditCard, PaymentOption.DebitCard],
-    ReservationAvailable: true,
-  };*/
