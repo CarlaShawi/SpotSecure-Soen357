e883c79a1272f20a281a5727d9eb89055986a96c
Tharsipa Nadarajah <90727247+Tharsipa@users.noreply.github.com>
2024-04-14 12:39:35 -0400

    Added notification bell

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,37 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 import { UserDTO } from '../../models/users';
+import { NotificationService } from 'src/app/services/notification.service';
+import { Subscription } from 'rxjs';
+import { AppNotification } from 'src/app/models/notifications';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   isLoggedIn = false;
   userName = '';
-  showMenu: boolean = false;
+  hasUnreadNotifications: boolean = false;
+  notifications: AppNotification[] = [];
+  private notificationsSubscription!: Subscription;
 
   constructor(
     private authService: AuthService,
     private userService: UserService,
-    private router: Router
+    private notificationService: NotificationService
   ) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.authService.afAuth.authState.subscribe((user) => {
       if (user) {
         this.isLoggedIn = true;
         this.userService.myUser.subscribe((userDetails: UserDTO | null) => {
           this.userName = userDetails ? userDetails.Email : '';
+          this.notificationService.fetchNotification(1);
         });
       } else {
         this.isLoggedIn = false;
+        this.notifications = [];
       }
     });
+
+    this.notificationsSubscription = this.notificationService
+      .getNotifications()
+      .subscribe((notifications: AppNotification[]) => {
+        this.notifications = notifications;
+        this.hasUnreadNotifications = notifications.length > 0;
+      });
   }
+
+  ngOnDestroy(): void {
+    if (this.notificationsSubscription) {
+      this.notificationsSubscription.unsubscribe();
+    }
+  }
+
+  onNotificationClick(notificationId: number): void {
+    this.notificationService.clearNotification(notificationId);
+  }
+
   async logOut() {
     this.userService.updateUser(null);
     await this.authService.SignOut();
